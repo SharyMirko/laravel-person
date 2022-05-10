d30babba1b16572585fa264fdaecaebcefdf2ed2
giuseppemirizzi <giuseppe.m1995@gmail.com>
2022-05-10 18:22:07 +0200

    ciclo foreach

--- database/seeds/PersonSeeder.php
+++ database/seeds/PersonSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Persons;
 
 class PersonSeeder extends Seeder
 {
@@ -11,6 +12,39 @@ class PersonSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $people=[
+            [
+                'first_name'=>'Giuseppe',
+                'last_name'=>'Mirizzi',
+                'age'=>27
+            ],
+            [
+                'first_name'=>'Mirko',
+                'last_name'=>'Moschella',
+                'age'=>27                                                                                                                                    
+            ],
+            [
+                'first_name'=>'Aldo',
+                'last_name'=>'Coccurullo',
+                'age'=>18                                                                                                                                                                                                                        
+            ],
+            [
+                'first_name'=>'Camilla',
+                'last_name'=>'Palombi',
+                'age'=>25
+            ],
+            [
+                'first_name'=>'Anthony',
+                'last_name'=>'Cecconi',
+                'age'=>26
+            ]
+        ];
+        foreach ($people as $person) {
+            $newPerson= new Persons();
+            $newPerson->first_name=$person['first_name'];
+            $newPerson->last_name=$person['last_name'];
+            $newPerson->age=$person['age'];
+            $newPerson->save();
+        };
     }
 }
